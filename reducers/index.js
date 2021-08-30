f313ef8183cc8bdbdeea4c4a8c9c9f79bb3d8315
Filip Glura <lockontargt@gmail.com>
2021-08-30 10:36:22 +0200

    feat: added dummy data for reducer

--- reducers/index.js
+++ reducers/index.js
@@ -1,5 +1,20 @@
 const initialState = {
     expenses: [
+        {
+            id: 0,
+            title: "Shopping",
+            cost: 70
+        },
+        {
+            id: 1,
+            title: "Fuel",
+            cost: 30
+        },
+        {
+            id: 2,
+            title: "House rent",
+            cost: 100
+        }
     ],
 };
 
