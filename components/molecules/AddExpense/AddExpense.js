ee39e5d7f7ea77b8aeb0cb4f60d1a466524c9d74
TARGET <lockontargt@gmail.com>
2021-09-02 10:21:33 +0200

    feat: added Yup and AddExpense validation

--- components/molecules/AddExpense/AddExpense.js
+++ components/molecules/AddExpense/AddExpense.js
@@ -3,13 +3,27 @@ import { Heading } from "components/atoms/Heading/Heading";
 import { connect } from "react-redux";
 import { useFormik } from "formik";
 import { addExpense as addExpenseContent } from "actions";
+import { Paragraph } from "components/atoms/Paragraph/Paragraph";
+import * as Yup from "yup";
 
 const AddExpense = ({ addExpense }) => {
+    const validationSchema = Yup.object().shape({
+        name: Yup.string()
+            .max(70, "Too many characters")
+            .required("Name field is required"),
+        cost: Yup.number()
+            .max(1000000, "Not too much?")
+            .integer("Cost must be a number")
+            .positive("Cost must be positive")
+            .required("Cost field is required"),
+    });
+
     const formik = useFormik({
         initialValues: {
             name: "",
             cost: "",
         },
+        validationSchema,
         onSubmit: (values) => {
             addExpense(values);
             formik.resetForm(); 
@@ -28,6 +42,7 @@ const AddExpense = ({ addExpense }) => {
                     value={formik.values.name}
                     className={styles.input}
                 />
+                <Paragraph>{formik.errors.name}</Paragraph>
                 <input
                     id="cost"
                     name="cost"
@@ -37,6 +52,7 @@ const AddExpense = ({ addExpense }) => {
                     value={formik.values.cost}
                     className={styles.input}
                 />
+                <Paragraph>{formik.errors.cost}</Paragraph>
                 <button type="submit" className={styles.add}>
                     Add
                 </button>
